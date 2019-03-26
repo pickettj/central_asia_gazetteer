2b8c0b617ed569a91dcf0661c16f768ddb41598c
JonMEngel <engel.jonathan98@gmail.com>
2019-03-25 20:45:20 -0400

    Trying to get test vector file to work; having trouble w/ Ajax, will talk to Gabi about it on Wednesday

--- rsrc/js/bukhara_protectorate_map.js
+++ rsrc/js/bukhara_protectorate_map.js
@@ -4,9 +4,12 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoicGlja2V0dGoiLCJhIjoiY2pzM2Q4OTc4MmVpZjQzbm13azhocmMxciJ9.pS0odo8k59Q7-I9-aMzwVg'
+    accessToken: 'pk.eyJ1IjoiZW5nZWxqb25hdGhhbjk4IiwiYSI6ImNqc3hpdmp0dTB0MXM0NW9kenlzN3p2ODgifQ.Z20FPmAIka2qYhuCYOQosA'
 }).addTo(mymap);
 
+var vectorFileLayer = L.geoJSON.ajax("\rsrc\vector-files\bukhar_protectorate.json.geojson");
+vectorFileLayer.addTo(mymap);
+//James's test code
 //not sure why these aren't working below
 
 var marker = L.marker([39.77691, 64.41315]).addTo(mymap);
