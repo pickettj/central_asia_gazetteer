e59cb9cce5b9114be6270a14383fa14cc60014dd
pickettj <pickettj@gmail.com>
2019-04-04 16:27:32 -0400

    major spruce up on About page, new content

---
+++ rsrc/js/simple_map_window.js
@@ -0,0 +1,8 @@
+var mymap = L.map('mapid').setView([39.77691, 64.41315], 8);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoicGlja2V0dGoiLCJhIjoiY2pzM2Q4OTc4MmVpZjQzbm13azhocmMxciJ9.pS0odo8k59Q7-I9-aMzwVg', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoicGlja2V0dGoiLCJhIjoiY2pzM2Q4OTc4MmVpZjQzbm13azhocmMxciJ9.pS0odo8k59Q7-I9-aMzwVg'
+}).addTo(mymap);
